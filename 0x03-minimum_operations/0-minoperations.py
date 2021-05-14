bcaae75877aacd85cee7c922b5f7d2a071df2756
Leidysalda <leidysalda1@gmail.com>
2021-05-14 02:04:38 +0000

    [Add]file 0. Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Minimum Operations
+"""
+
+def minOperations(n):
+    """
+    In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste.\
+    Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    """
+    aux = 0
+    copy = 1
+    count = 0
+
+    while copy < n:
+        if n % copy == 0:
+            aux = copy
+            copy *= 2
+            count += 1
+        else:
+            copy += aux
+
+        count += 1
+    return count
+
+

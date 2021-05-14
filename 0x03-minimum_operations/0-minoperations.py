eb63a794497b3d4ad475e45eb271bd9582ef5bfd
Leidysalda <leidysalda1@gmail.com>
2021-05-14 02:12:11 +0000

    [Upload] Pep8 style

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,10 +2,13 @@
 """Minimum Operations
 """
 
+
 def minOperations(n):
     """
-    In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste.\
-    Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste.Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
     """
     aux = 0
     copy = 1
@@ -21,5 +24,3 @@ def minOperations(n):
 
         count += 1
     return count
-
-

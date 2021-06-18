6ae39c88ce3d11d7f5081c48819e5cbdb1c4fc4d
leidysalda <leidysalda1@gmail.com>
2021-06-18 03:17:34 +0000

    [Update]file 0-lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
     """ Locked boxes """
     keys = [0]
 
     if boxes is None or boxes == []:
         return False
-    
     for k in keys:
         for i in boxes[k]:
             if i not in keys:
                 if i < len(boxes):
                     keys.append(i)
-
     if len(keys) != len(boxes):
         return False
     return True

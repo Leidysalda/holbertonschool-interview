234e16c5906743d2b8388bbc9cb1076746c4df5f
Leidysalda <leidysalda1@gmail.com>
2021-07-03 04:29:10 +0000

    [Add]files 1-heap_insert.c, 0-binary_tree_node.c

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -13,19 +13,27 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+
+typedef struct binary_tree_s
 {
 int n;
 struct binary_tree_s *parent;
 struct binary_tree_s *left;
 struct binary_tree_s *right;
-};
+} binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+heap_t *heap_insert(heap_t **root, int value);
 void binary_tree_print(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_validation(binary_tree_t *tree, int value);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
 
 #endif

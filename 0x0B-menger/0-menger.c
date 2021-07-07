812adc0bc41a84e4a5ee2465598ac0231d90ac73
Leidysalda <leidysalda1@gmail.com>
2021-07-07 20:41:25 +0000

    [Fix] Betty style file 0-menger.c

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -18,32 +18,31 @@ return;
 lev = (int)pow(3, level);
 
 for (i = 0; i < lev; i++)
-  {
-    for (j = 0; j < lev; j++)
-      {
-	if (i % 3 == 1 && j % 3 == 1)
-	  {
-	    printf(" ");
-	    continue;
-	  }
-	if (lev / 3 <= i && 2 * (lev / 3) > i)
-	  {
-	    if (lev / 3 <= j && 2 * (lev / 3) > j)
-	      {
-		printf(" ");
-		continue;
-	      }
-	  }
-	if (validate_one(lev, i, j))
-	  continue;
+{
+for (j = 0; j < lev; j++)
+{
+if (i % 3 == 1 && j % 3 == 1)
+{
+printf(" ");
+continue;
+}
+if (lev / 3 <= i && 2 * (lev / 3) > i)
+{
+if (lev / 3 <= j && 2 * (lev / 3) > j)
+{
+printf(" ");
+continue;
+}
+}
+if (validate_one(lev, i, j))
+continue;
+if (validate_two(i, j))
+continue;
 
-	if (validate_two(i, j))
-	  continue;
-
-	printf("#");
-      }
-    printf("\n");
-  }
+printf("#");
+}
+printf("\n");
+}
 }
 
 
@@ -59,23 +58,23 @@ for (i = 0; i < lev; i++)
 
 int validate_one(int lev, int i, int j)
 {
-  int a, b;
+int a, b;
 
-  for (a = 1; a < lev / 3; a += 3)
-    {
-      for (b = 1; b < lev / 3;  b +=3)
-	{
-	  if ((lev / 9) * a <= i && ((lev / 9) * a) + lev / 9 > i)
-	    {
-	      if ((lev / 9) * b <= j && ((lev / 9) * b) + lev / 9 > j)
-		{
-		  printf(" ");
-		  return (1);
-		}
-	    }
-	}
-    }
-  return (0);
+for (a = 1; a < lev / 3; a += 3)
+{
+for (b = 1; b < lev / 3;  b += 3)
+{
+if ((lev / 9) * a <= i && ((lev / 9) * a) + lev / 9 > i)
+{
+if ((lev / 9) * b <= j && ((lev / 9) * b) + lev / 9 > j)
+{
+printf(" ");
+return (1);
+}
+}
+}
+}
+return (0);
 }
 
 
@@ -90,20 +89,20 @@ int validate_one(int lev, int i, int j)
 
 int validate_two(int i, int j)
 {
-  int k, l;
+int k, l;
 
-  for (k = 3; k < 6; k++)
-    {
-      for (l = 3; l < 6; l++)
-	{
-	  if (i % 9 == k && j % 9 == l)
-	    {
-	      printf(" ");
-	      return (1);
-	    }
-	}
-    }
-  return (0);
+for (k = 3; k < 6; k++)
+{
+for (l = 3; l < 6; l++)
+{
+if (i % 9 == k && j % 9 == l)
+{
+printf(" ");
+return (1);
+}
+}
+}
+return (0);
 }
 
 

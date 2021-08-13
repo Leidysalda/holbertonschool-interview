c555d320b248f3b2f9455c5b4e1f50f0c2c14534
vagrant <vagrant@vagrant.vm>
2021-08-13 02:27:40 +0000

    [Fix]file 0-

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,17 +7,17 @@ const film = process.argv[2];
 
 // Query
 request(api + film, (error, response, body) => {
-    if (error) throw error;
-    const characters = JSON.parse(body).characters;
-    viewNames(characters);
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  viewNames(characters);
 });
 
 //Results
 const viewNames = (names, i = 0) => {
-    if (i === names.length) return;
-    request(names[i], (error, response, body) => {
-	if (error) throw error;
-	console.log(JSON.parse(body).name);
-	viewNames(names, i + 1);
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    viewNames(names, i + 1);
     });
 };

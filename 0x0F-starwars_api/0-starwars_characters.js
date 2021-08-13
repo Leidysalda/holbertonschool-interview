573e6ebea1f8587a7c9e5bb6ffbcd1ee4cff5cb3
vagrant <vagrant@vagrant.vm>
2021-08-13 02:18:44 +0000

    [Add]file 0-starwars_characters.js

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+const api = 'https://swapi-api.hbtn.io/api/films/';
+const film = process.argv[2];
+
+
+request(api + film, (error, response, body) => {
+    if (error) throw error;
+    const characters = JSON.parse(body).characters;
+    viewNames(characters);
+});
+
+const viewNames = (names, i = 0) => {
+    if (i === names.length) return;
+    request(names[i], (error, response, body) => {
+	if (error) throw error;
+	console.log(JSON.parse(body).name);
+	viewNames(names, i + 1);
+    });
+};

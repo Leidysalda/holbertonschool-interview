7625b288cd955f4ee168b281437bc835dffeb44b
leidysalda <leidysalda1@gmail.com>
2021-09-02 13:07:12 +0000

    [Fix] Betty style in advanced_binary

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - function that searches for a value in a sorted array of integers.
+ * advanced_binary - function that searches for a value in
+ * a sorted array of integers.
  *
  * @array: array
  * @size: size array
@@ -12,34 +13,34 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-  int h = ((size - 1) / 2);
-  int result = 0;
-  size_t i = 0;
+int h = ((size - 1) / 2);
+int result = 0;
+size_t i = 0;
 
-  if (array == NULL || size < 1)
-    return (-1);
-      
-  printf("Searching in array: ");
-  for (i = 0; i < size; i++){
-    printf("%d", array[i]);
-    if (i != (size -1))
-      printf(", ");
-    else
-      printf("\n");
-  }
+if (array == NULL || size < 1)
+return (-1);
 
-  if (array[h] == value && h == 0)
-    return (h);
-  else if (size == 1)
-    return (-1);
+printf("Searching in array: ");
+for (i = 0; i < size; i++)
+{
+printf("%d", array[i]);
+if (i != (size - 1))
+printf(", ");
+else
+printf("\n");
+}
 
-  if (array[h] >= value)
-    return (advanced_binary(array, h+1, value));
+if (array[h] == value && h == 0)
+return (h);
+else if (size == 1)
+return (-1);
 
-  h++;
-  result = advanced_binary(&array[h], size - h, value);
-  if (result == -1)
-    return (-1);
-  return (h + result);
-    
+if (array[h] >= value)
+return (advanced_binary(array, h + 1, value));
+
+h++;
+result = advanced_binary(&array[h], size - h, value);
+if (result == -1)
+return (-1);
+return (h + result);
 }

06897207738600e53038afccd5da11c997d55a4f
Leidysalda <leidysalda1@gmail.com>
2021-10-13 03:40:33 +0000

    [Update] pycodestyle 0-rotate

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Given an n x n 2D matrix, rotate it 90 degrees clockwise.
@@ -11,11 +12,11 @@ def rotate_2d_matrix(matrix):
 
     for row in range(int(long / 2)):
         offset = 0
-        i = long -1 - row
+        i = long - 1 - row
         for column in range(row, long - 1 - row):
             top = matrix[row][column]
             matrix[row][column] = matrix[i - offset][row]
-            matrix[i - offset][row]= matrix[i][i - offset]
+            matrix[i - offset][row] = matrix[i][i - offset]
             matrix[i][i - offset] = matrix[column][i]
             matrix[column][i] = top
             offset += 1

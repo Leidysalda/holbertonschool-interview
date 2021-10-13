17ffd97e230e7adee8aafd559fde0dfc3f3202ca
Leidysalda <leidysalda1@gmail.com>
2021-10-13 03:54:57 +0000

    [Add]file

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,6 +8,7 @@ def count_words(subreddit, word_list, kw_count={}, next_pg=None, reap_kw={}):
     """ Count words """
     headers = {"User-Agent": "Leidysalda"}
 
+    # next page
     if next_pg:
         subRhot = requests.get('https://reddit.com/r/' + subreddit +
                                '/hot.json?after=' + next_pg,
@@ -16,19 +17,23 @@ def count_words(subreddit, word_list, kw_count={}, next_pg=None, reap_kw={}):
         subRhot = requests.get('https://reddit.com/r/' + subreddit +
                                '/hot.json', headers=headers)
 
+    # Status 404
     if subRhot.status_code == 404:
         return
 
+    # Count
     if kw_count == {}:
         for word in word_list:
             kw_count[word] = 0
             reap_kw[word] = word_list.count(word)
 
+    # File json
     subRhot_dict = subRhot.json()
     subRhot_data = subRhot_dict['data']
     next_pg = subRhot_data['after']
     subRhot_posts = subRhot_data['children']
 
+    # Post data
     for post in subRhot_posts:
         post_data = post['data']
         post_title = post_data['title']
@@ -37,7 +42,7 @@ def count_words(subreddit, word_list, kw_count={}, next_pg=None, reap_kw={}):
             for key in kw_count:
                 if w.lower() == key.lower():
                     kw_count[key] += 1
-
+    # Next page
     if next_pg:
         count_words(subreddit, word_list, kw_count, next_pg, reap_kw)
 
@@ -46,9 +51,11 @@ def count_words(subreddit, word_list, kw_count={}, next_pg=None, reap_kw={}):
             if v > 1:
                 kw_count[key] *= v
 
+        # Sorted
         sorted_abc = sorted(kw_count.items(), key=lambda x: x[0])
         sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
 
+        # Print sorted
         for res in sorted_res:
             if res[1] > 0:
                 print(f"{res[0].lower()}: {res[1]}")

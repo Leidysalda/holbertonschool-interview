ac33089b449f7f4c82be53f96200f49a1971cbac
Leidysalda <leidysalda1@gmail.com>
2021-10-14 03:58:09 +0000

    [Add]files

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - sorted array
+ *
+ * @array: The array to be printed
+ * @size: Size of the array
+ *
+ * Return: Pointer or NULL
+ */
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+avl_t *tree;
+
+if (array == NULL)
+return (NULL);
+
+tree = search_avl(array, 0, size - 1, NULL);
+return (tree);
+}
+
+/**
+ * search_avl - search avl
+ *
+ * @array: array
+ * @first: start array
+ * @last: end array
+ * @node: node
+ *
+ * Return: 0 on success, error code on failure
+ */
+
+avl_t *search_avl(int *array, int first, int last, avl_t *node)
+{
+int half;
+avl_t *new_node;
+
+if (firts > last)
+return (NULL);
+
+new_node = malloc(sizeof(avl_t));
+if (new_node == NULL)
+return (NULL);
+
+half = (first + last) / 2;
+new_node->n = arra[halt];
+new_node->parent = node;
+new_node->left = search_avl(array, first, halt - 1, new_node);
+new_node->right = search_avl(array, halt + 1, last, new_node);
+return (new_node);
+}
